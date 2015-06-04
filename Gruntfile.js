92115e5e1adc7b20b1a18095af9e55214e90db41
Dominic Watson <watson.dominic@gmail.com>
2015-06-04 10:26:10 +0100

    Zipping up builds for pushing to S3

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,17 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks( 'grunt-exec'                );
 	grunt.loadNpmTasks( 'grunt-wget'                );
 	grunt.loadNpmTasks( 'grunt-zip'                 );
+	grunt.loadNpmTasks( 'grunt-zipstream'           );
 	grunt.loadNpmTasks( 'grunt-contrib-clean'       );
+	grunt.loadNpmTasks( 'grunt-contrib-copy'        );
 
-	grunt.registerTask( 'default', [ 'wget:undertowserverjar', 'exec:nwgyp', 'nodewebkit'         ] );
+	grunt.registerTask( 'default', [ 'wget:undertowserverjar', 'exec:nwgyp', 'nodewebkit' ] );
+	grunt.registerTask( 'zip'    , [ 'exec:zip_linux_32' , 'exec:zip_linux_64', 'exec:zip_windows_32' , 'exec:zip_windows_64', 'exec:zip_osx_32' , 'exec:zip_osx_64', 'copy:zips' ] );
 	grunt.registerTask( 'jars'   , [ 'wget:undertowserverjar', 'wget:luceejars', 'unzip', 'clean' ] );
-	grunt.registerTask( 'linux'  , [ 'nodewebkit:linux'                                     ] );
-	grunt.registerTask( 'win'    , [ 'nodewebkit:win'                                       ] );
-	grunt.registerTask( 'osx'    , [ 'nodewebkit:osx'                                       ] );
-	grunt.registerTask( 'nwgyp'  , [ 'exec:nwgyp'                                           ] );
+	grunt.registerTask( 'linux'  , [ 'nodewebkit:linux'                                           ] );
+	grunt.registerTask( 'win'    , [ 'nodewebkit:win'                                             ] );
+	grunt.registerTask( 'osx'    , [ 'nodewebkit:osx'                                             ] );
+	grunt.registerTask( 'nwgyp'  , [ 'exec:nwgyp'                                                 ] );
 
 	grunt.initConfig( {
 		nodewebkit: {
@@ -25,6 +28,42 @@ module.exports = function (grunt) {
 			nwgyp : {
 				command : 'nw-gyp configure --target=0.11.6 && nw-gyp build',
 				cwd     : './app/node_modules/java',
+				stdout  : true,
+				stderr  : true
+			},
+			zip_linux_32 : {
+				command : 'zip linux32.zip *',
+				cwd     : './builds/lucee-scratchpad/linux32',
+				stdout  : true,
+				stderr  : true
+			},
+			zip_linux_64 : {
+				command : 'zip linux64.zip *',
+				cwd     : './builds/lucee-scratchpad/linux64',
+				stdout  : true,
+				stderr  : true
+			},
+			zip_osx_32 : {
+				command : 'zip osx32.zip *',
+				cwd     : './builds/lucee-scratchpad/osx32',
+				stdout  : true,
+				stderr  : true
+			},
+			zip_osx_64 : {
+				command : 'zip osx64.zip *',
+				cwd     : './builds/lucee-scratchpad/osx64',
+				stdout  : true,
+				stderr  : true
+			},
+			zip_windows_32 : {
+				command : 'zip win32.zip *',
+				cwd     : './builds/lucee-scratchpad/win32',
+				stdout  : true,
+				stderr  : true
+			},
+			zip_windows_64 : {
+				command : 'zip win64.zip *',
+				cwd     : './builds/lucee-scratchpad/win64',
 				stdout  : true,
 				stderr  : true
 			}
@@ -47,6 +86,19 @@ module.exports = function (grunt) {
 			'./app/server-resources/lucee4-lib/' : 'lucee-4.5.1.000-jars.zip'
 		},
 
-		clean: [ "lucee-4.5.1.000-jars.zip" ]
+		clean: [ "lucee-4.5.1.000-jars.zip" ],
+
+		copy: {
+			zips: {
+				files: [
+					{ src: [ 'builds/lucee-scratchpad/linux32/linux32.zip' ], dest: 'builds/lucee-scratchpad/zips/linux32.zip' },
+					{ src: [ 'builds/lucee-scratchpad/linux64/linux64.zip' ], dest: 'builds/lucee-scratchpad/zips/linux64.zip' },
+					{ src: [ 'builds/lucee-scratchpad/win32/win32.zip'     ], dest: 'builds/lucee-scratchpad/zips/win32.zip'   },
+					{ src: [ 'builds/lucee-scratchpad/win64/win64.zip'     ], dest: 'builds/lucee-scratchpad/zips/win64.zip'   },
+					{ src: [ 'builds/lucee-scratchpad/osx32/osx32.zip'     ], dest: 'builds/lucee-scratchpad/zips/osx32.zip'   },
+					{ src: [ 'builds/lucee-scratchpad/osx64/osx64.zip'     ], dest: 'builds/lucee-scratchpad/zips/osx64.zip'   }
+				]
+			},
+		}
 	} );
 };
